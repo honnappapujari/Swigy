db73fe4e0e9b6f4554645ed5d10cef7ae5b11fe1
honnappapujari <pujarihonnappa888@gmail.com>
2024-06-02 08:12:42 +0530

    First step

--- index.js
+++ index.js
@@ -1,8 +1,64 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
+import logo from './assets/food-logo.png';
 
 const createEl = React.createElement('h1', {}, 'hello world');
 
+const Header = () =>
+{
+    return (
+        <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+        <a className="navbar-brand"><img height={100} src={logo}/></a>
+        <ul className="nav justify-content-end">
+            <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Active</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+            </li>
+        </ul>
+        </div>
+      </nav>
+    )
+}
+
+const RestroCard = () =>
+{
+    return(
+        <div className="resto-card">
+            <h1>Meghana Food </h1>
+        </div>
+    )
+}
+
+const Body = () =>
+{
+    return (
+        <div className="body">
+            <div className="restro-container">
+               <RestroCard />
+            </div>
+        </div>
+    )
+}
+const AppLayout = () =>
+{
+    return (<div id="Container">
+            <Header/>
+            <Body />
+    </div>
+    
+    )
+}
+
+
 const createRoot = ReactDOM.createRoot(document.getElementById('root'));
 
-createRoot.render(createEl);
+createRoot.render(<AppLayout/>);
