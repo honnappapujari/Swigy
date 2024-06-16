9e059ff014457b5f54b5f6ed24fe4439ad15e182
honnappapujari <pujarihonnappa888@gmail.com>
2024-06-16 19:23:28 +0530

    Use state hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,29 @@
 import RestroCard from './Restrocard';
 import {resObj} from '../utils/mockdata'
+import { useState } from 'react';
 
 const Body = () =>
 {
+    const [usestateList, setUseStateList] = useState(resObj);
     return (
         <div className="body">
             <div className='form-group'>
-                <button type='button' className='btn btn-sm btn-primary' onClick={() => console.log('btn clicked')} onMouseOver={() => console.log('hovering')}>****</button>
+                <button type='button' className='btn btn-sm btn-primary' onClick={ () =>
+                {
+                    const newList = resObj.filter((restorent) => restorent.info.avgRating < 4);
+                    setUseStateList(newList)
+
+                } 
+                    
+                }
+                >****</button>
             </div>
             <div className="restro-container grid gap-3">
                 {/* Note :Dont use index as key */}
-                {resObj.map((restorent) => <RestroCard key={restorent.info.id} resData ={restorent} />)}           
+                {
 
-
+usestateList.map((restorent) => <RestroCard key={restorent.info.id} resData ={restorent} />)}           
+                
             </div>
         </div>
     )
