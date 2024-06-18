25961f51ec71f81dce85a38bb8943ffc834e1187
honnappapujari <pujarihonnappa888@gmail.com>
2024-06-18 17:13:45 +0530

    shimmer UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,24 @@
 import RestroCard from './Restrocard';
 import {resObj} from '../utils/mockdata'
+import { ShimmerSimpleGallery } from "react-shimmer-effects";
+
 import { useState, useEffect } from 'react';
 
 const Body = () =>
 {
-    const [usestateList, setUseStateList] = useState(resObj);
-    useEffect(() => {getData(), []});
+    const [usestateList, setUseStateList] = useState([]);
+    useEffect(() => {getData()}, []);
 
     const getData = async() =>
     {
         const data =  await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
     const json = await data.json();
-  
     setUseStateList(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+    }
+    if(usestateList == 0)
+    {
+            return <ShimmerSimpleGallery card imageHeight={300} caption />
+          
     }
     return (
         <div className="body">
@@ -27,7 +33,7 @@ const Body = () =>
                 }
                 >****</button>
             </div>
-            <div className="restro-container grid gap-3">
+            <div className="restro-container">
                 {/* Note :Dont use index as key */}
                 {
 usestateList.map((restorent) => <RestroCard key={restorent.info.id} resData ={restorent} />)}           
---
+++ src/components/Shimmer.js
@@ -0,0 +1,12 @@
+export const  ShimmerUI = () =>
+{
+    return(
+        <div className="shimmer-container">
+            <div className="shimmer-cards">Shimmer</div>
+            <div className="shimmer-cards">Shimmer</div>
+            <div className="shimmer-cards">Shimmer</div>
+            <div className="shimmer-cards">Shimmer</div>
+        
+        </div>
+    )
+}
