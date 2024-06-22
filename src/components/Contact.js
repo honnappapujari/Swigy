0a9fde9f4f294c0ee794e6a91c9818b1134caab7
honnappapujari <pujarihonnappa888@gmail.com>
2024-06-22 22:52:39 +0530

    Contact us page

---
+++ src/components/Contact.js
@@ -0,0 +1,9 @@
+export const Contact = () =>
+{
+    return(
+        <>
+        <h1>Contact page</h1>
+        <p>This is contact us page</p>
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import logo from '../assets/food-logo.png';
 import {Header} from './components/Header';
 import Body from './components/Body';
 import { About } from "./components/About";
+import { Contact } from "./components/Contact";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const createEl = React.createElement('h1', {}, 'hello world');
@@ -25,6 +26,9 @@ const Approuter = createBrowserRouter([
     },
     {
         path: '/about', element:<About/>
+    },
+    {
+        path : '/contact', element: <Contact />
     }
 ])
 
