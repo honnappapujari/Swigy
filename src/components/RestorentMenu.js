1e610fcd0fc07298f8e312cc568f395b307eb354
honnappapujari <pujarihonnappa888@gmail.com>
2024-06-26 21:41:45 +0530

    #

--- src/components/RestorentMenu.js
+++ src/components/RestorentMenu.js
@@ -1,17 +1,26 @@
+import { useEffect,useState } from "react"
+import { ShimmerCard, ShimmerCategoryList } from "react-shimmer-effects";
 export const RestorentMenu = () =>
 {
-    return(
-        <>
-        <h1>Name of restorent</h1>
-        <h2>Menu</h2>
-        <ul>
-            <li>
-                Menu 1
-            </li>
-            <li>
-                Menu 2
-            </li>
-        </ul>
+    const [restInfo, setresInfo] = useState(null);
+    useEffect(() =>
+    {
+        getRespnse();
+    }, [])
+
+     getRespnse = async() =>
+    {
+        const data = await fetch('https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.96340&lng=77.58550&restaurantId=719639&catalog_qa=undefined&submitAction=ENTER');
+        const json = await data.json();
+        console.log(json)
+        setresInfo(json)
+    }
+    const url =restInfo?.data?.cards[2]?.card?.card?.info;
+    return restInfo == null ?(<h1>Loading....</h1>):(
+    <>
+        <h1>{url?.name}</h1>
+        <h2>{url?.costForTwoMessage}</h2>
+        <p>{url.cuisines.join(', ')}</p>
         </>
     )
 }
